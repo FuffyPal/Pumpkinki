20b1aeb793ab50c7a1d96664223d54f20620fdcb
Flaouve <flaouve@gmail.com>
2026-06-29 06:08:21 +0000

    feat: add os detection and add find architecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+fn os_detection() -> i8 {
+    if cfg!(target_os = "windows") {
+        1 // windows
+    } else if cfg!(target_os = "linux") {
+        2 // linux
+    } else if cfg!(target_os = "macos") {
+        3 // macos
+    } else {
+        0 // bsd etc.
+    }
+}
+
+fn find_architecture() -> i8 {
+    if cfg!(target_arch = "x86_64") {
+        1 // intel & amd
+    } else if cfg!(target_arch = "x86") {
+        2 // intel & amd but 32-bit
+    } else if cfg!(target_arch = "arm") {
+        3 // arm 32-bit
+    } else if cfg!(target_arch = "aarch64") {
+        4 // m1,m2,m3,m4 and arm v8+bit
+    } else {
+        0 // risc-v etc.
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("OS Code: {}", os_detection());
+    println!("Architecture Code: {}", find_architecture());
 }
